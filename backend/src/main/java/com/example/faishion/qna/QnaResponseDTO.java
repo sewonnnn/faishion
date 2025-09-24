7ed75fbf6300795d8c463af3ca10f10063e68e33
hyunho <smz1722@naver.com>
2025-09-25 00:23:07 +0900

    AI 사용자 이미지 피팅 완료, qna 판매자 토큰만 불러오면 완료

--- backend/src/main/java/com/example/faishion/qna/QnaResponseDTO.java
+++ backend/src/main/java/com/example/faishion/qna/QnaResponseDTO.java
@@ -1,6 +1,7 @@
 package com.example.faishion.qna;
 
 // ✅ QnaResponseDTO.java 추가 (목록 조회용)
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,10 @@ public class QnaResponseDTO {
     private String userName; // 작성자 이름
     private String title;
     private String content;
+    private String answer;
+    @JsonProperty("isSecret")
     private boolean isSecret; // 비밀글 여부
     private String createdAt;
+    @JsonProperty("isMine")
+    private boolean isMine; // 로그인 사용자와 작성자 사용자 일치여부
 }
