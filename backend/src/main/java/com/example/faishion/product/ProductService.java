13bb8accd143828a59942a691cafe0bf39a656d2
xorwns <xorwns56@naver.com>
2025-09-26 09:35:19 +0900

    머지 후 오류수정

--- backend/src/main/java/com/example/faishion/product/ProductService.java
+++ backend/src/main/java/com/example/faishion/product/ProductService.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
